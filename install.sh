9b9c0ea80090181b86156e4e7c18d4cb7f3fa4a7
Henry <lehieu@proxyme.link>
2025-06-05 09:22:40 +0700

    simplify less trash terminak

--- install.sh
+++ install.sh
@@ -7,14 +7,10 @@ set -e
 REPO_URL="https://raw.githubusercontent.com/Glintek/xoa-routing-ports/main"
 
 # Download xoa-routing-ports file
-echo "Downloading xoa-routing-ports..."
+echo "Downloading content..."
 curl -sSL "${REPO_URL}/xoa-routing-ports" -o xoa-routing-ports
-
-# Download .vendor.default.conf file
-echo "Downloading .vendor.default.conf..."
 curl -sSL "${REPO_URL}/.vendor.default.conf" -o .vendor.default.conf
 
-# Make xoa-routing-ports executable
 chmod +x xoa-routing-ports
 
 echo "Installation completed successfully!"
